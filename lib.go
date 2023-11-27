20a4f9c3e341c03ef71d3b5e26e3f944bd602dac
Abiria <abiria.dev@gmail.com>
2023-11-27 13:55:58 +0900

    revert: use span instread
    This reverts commit 724cd638058644e82b6cbfded2fa5f0a864c1787.

--- lib.go
+++ lib.go
@@ -1,7 +1,7 @@
 package hango
 
 import (
-	"io"
+	"strings"
 	"text/scanner"
 )
 
@@ -9,15 +9,15 @@ type HangoLex struct {
 	s scanner.Scanner
 }
 
-func HangoLexNew(src io.Reader) HangoLex {
+func HangoLexNew(src string) HangoLex {
 	var lex HangoLex
-	lex.s.Init(src)
+	lex.s.Init(strings.NewReader(src))
 	lex.s.Mode = scanner.GoTokens &^ scanner.SkipComments
 	lex.s.Whitespace ^= 1<<' ' | 1<<'\t' | 1<<'\r' | 1<<'\n'
 	return lex
 }
 
-func (this *HangoLex) Scan() (rune, int, int, string) {
+func (this *HangoLex) Scan() (rune, int, int) {
 	tok := this.s.Scan()
-	return tok, this.s.Position.Offset, this.s.Pos().Offset, this.s.TokenText()
+	return tok, this.s.Position.Offset, this.s.Pos().Offset
 }
