b0b997506ed68aff39ed6e445160a7e62aa7638e
Abiria <abiria.dev@gmail.com>
2023-11-27 11:11:02 +0900

    feat: full lexer implementation

---
+++ lib.go
@@ -0,0 +1,23 @@
+package hango
+
+import (
+	"strings"
+	"text/scanner"
+)
+
+type HangoLex struct {
+	s scanner.Scanner
+}
+
+func HangoLexNew(src string) HangoLex {
+	var lex HangoLex
+	lex.s.Mode = scanner.GoTokens &^ scanner.SkipComments
+	lex.s.Whitespace ^= 1<<' ' | 1<<'\t' | 1<<'\r' | 1<<'\n'
+	lex.s.Init(strings.NewReader(src))
+	return lex
+}
+
+func (this *HangoLex) Init(src string) (rune, int, int) {
+	tok := this.s.Scan()
+	return tok, this.s.Position.Offset, this.s.Pos().Offset
+}
