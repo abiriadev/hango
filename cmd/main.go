350acde3668e14fbf7e895e83e709feac97e79c0
Abiria <abiria.dev@gmail.com>
2023-11-27 16:03:35 +0900

    feat: support multiple files

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/abiriadev/hango"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("pass a file to transform")
+	flag.Parse()
+
+	srcs := flag.Args()
+
+	if len(srcs) == 0 {
+		d, _ := io.ReadAll(os.Stdin)
+
+		r := hango.HangoLexNew(string(d)).RegenerateSource()
+
+		fmt.Print(r)
+	} else {
+
+		for _, src := range srcs {
+			ext := filepath.Ext(src)
+
+			if ext != ".hgo" {
+				panic("no hgo!")
+			}
+
+			target := strings.TrimSuffix(src, ext) + ".go"
+
+			d, e := os.ReadFile(src)
+
+			if e != nil {
+				panic(e)
+			}
+
+			r := hango.HangoLexNew(string(d)).RegenerateSource()
+
+			os.WriteFile(target, []byte(r), 0644)
+		}
 	}
-
-	d, _ := os.ReadFile(os.Args[1])
-
-	r := hango.HangoLexNew(string(d)).RegenerateSource()
-
-	fmt.Print(r)
 }
